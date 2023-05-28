86962c838ebcde082cd5674a6b075f5ee69ccb05
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-05-28 22:43:59 +0100

    Project initialized - mvn clean install successful

---
+++ src/main/java/com/samhcoco/projects/springcloudgatewaykubernetes/SpringCloudGatewayKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.samhcoco.projects.springcloudgatewaykubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayKubernetesApplication.class, args);
+	}
+
+}
